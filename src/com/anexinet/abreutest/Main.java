2557775845ab8d92a08b6917c6d166d6a423dc21
abreux <bior87@gmail.com>
2020-12-13 21:11:07 -0600

    Answer 3 convert numbers in different bases with validation

--- src/com/anexinet/abreutest/Main.java
+++ src/com/anexinet/abreutest/Main.java
@@ -2,19 +2,20 @@ package com.anexinet.abreutest;
 
 import com.anexinet.abreutest.question1.AnswerOne;
 import com.anexinet.abreutest.question2.AnswerTwo;
+import com.anexinet.abreutest.question3.AnswerThree;
 
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-
-        System.out.println("Question One add two numbers without aritmethic operator");
+        System.out.println("Question 1: add two numbers without aritmethic operator");
         System.out.println("5+11="+new AnswerOne().addTwoNumbers(5,11));
-        System.out.println("Get coincidences between 2 strings");
+        System.out.println("Question 2: Get coincidences between 2 strings");
         System.out.println("asdfghjwertyuicvbn  and digezponc, coincidences: ");
         Character []coincidences =new AnswerTwo().getCoincidencesInTwoString("asdfghjwertyuicvbn","digezponc");
         Arrays.stream(coincidences).forEach(element->System.out.print(element));
+        System.out.println("Question 3: convert number in a given base to int");
+        System.out.println("FFF is equivalent to "+ new AnswerThree().converToDecimal("FFF",16));
     }
 }
---
+++ src/com/anexinet/abreutest/question3/AnswerThree.java
@@ -0,0 +1,42 @@
+package com.anexinet.abreutest.question3;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class AnswerThree {
+
+    //This map contains regular expressions to validate characters for the diven base
+    private static Map<Integer, String> baseValidationRegex = Stream.of(new Object[][]{
+            {2, "^[0-1]+$"},
+            {8, "^[0-7]+$"},
+            {10, "^[0-9]+$"},
+            {16, "^[A-F0-9]+$"}
+    }).collect(Collectors.toMap(p -> (Integer)p[0], p -> (String)p[1]));
+
+    /**
+     * Converts String in Base X to integer, with X in {2, 8, 10, 16}
+     * @param number String with the number to be converted
+     * @param base
+     * @return int number converted, -1 if the conversion was not possible
+     */
+    public int converToDecimal(String number, int base) {
+        number = number.toUpperCase(Locale.ROOT);
+        int result = -1;
+        if(baseValidationRegex.containsKey(base)){
+            Pattern pattern = Pattern.compile(baseValidationRegex.get(base));
+            Matcher matcher = pattern.matcher(number);
+            if(matcher.matches()){
+                result = Integer.parseInt(number,base);
+            }else{
+                System.out.println("The number "+number+" doesn't have the correct format for base "+base);
+            }
+        }else{
+            System.out.println("Base not supported, try with 2,8, 10 or 16");
+        }
+        return  result;
+    }
+}
